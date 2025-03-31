b035a263da9968a149e543ef390dedca861a2960
arcasali21 <afcasali@comunidad.unnoba.edu.ar>
2025-03-31 19:18:12 -0300

    TP1 de PDC  - Ej 5 - funcionando bien / creo

--- src/Ej_5_Filosofos_comensales/Filosofo.java
+++ src/Ej_5_Filosofos_comensales/Filosofo.java
@@ -7,16 +7,55 @@ public class Filosofo extends Thread {
     private Tenedor tenedor_derecha;
     private Tenedor tenedor_izquierda;
     private int tiempo_pensamiento = 0; //tiempo en ms en que el filosofo se la pasa pensando
+    private int tiempo_comiendo = 0; //tiempo en ms en que el filosofo se la pasa pensando
 
 
-    public Filosofo(String nombre, int tiempo_pensamiento, Tenedor tenedor_derecha, Tenedor tenedor_izquierda ) {
+    public Filosofo(String nombre, int tiempo_pensamiento, int tiempo_comiendo, Tenedor tenedor_derecha, Tenedor tenedor_izquierda ) {
         this.nombre = nombre;
         this.tiempo_pensamiento = tiempo_pensamiento;
+        this.tiempo_comiendo = tiempo_comiendo;
         this.tenedor_derecha = tenedor_derecha;
         this.tenedor_izquierda = tenedor_izquierda;
     }
-    public void run() {
 
+    public void run() {
+        while(true) {
+            System.out.println(this.nombre + " - Intenta comer ");
+            if(intentar_comer()){
+                System.out.println(this.nombre + " - COMIENDO ");
+                try {
+                    sleep(tiempo_comiendo);
+                } catch (InterruptedException e) {
+                    System.err.println("El hilo fue interrumpido: " + e.getMessage());
+                }
+                tenedor_derecha.liberar();
+                tenedor_izquierda.liberar();
+                System.out.println(this.nombre + " - termino de comer ");
+                this.pensando();
+            }else {
+                System.out.println(this.nombre + " - No tiene tenedores");
+                this.pensando();
+            }
+
+        }
+    }
+
+    private void pensando() {
+        System.out.println(this.nombre + "- PENSANDO ");
+        try {
+            sleep(tiempo_pensamiento);
+        } catch (InterruptedException e) {
+            System.err.println("El hilo fue interrumpido: " + e.getMessage());
+        }
+    }
+
+    public synchronized boolean intentar_comer() {
+        if(!tenedor_derecha.usando() && !tenedor_izquierda.usando()) {
+            tenedor_derecha.usar();
+            tenedor_izquierda.usar();
+            return true;
+        }else
+            return false;
     }
 
 }
--- src/Ej_5_Filosofos_comensales/Main.java
+++ src/Ej_5_Filosofos_comensales/Main.java
@@ -1,7 +1,28 @@
 package Ej_5_Filosofos_comensales;
 
+import java.util.Random;
+
 public class Main {
+
+    private static final int NUM_FILOSOFOS = 5;
+    private static final int TIEMPO_PESAMIENTO_MAX = 10000;
+    private static final int TIEMPO_COMIENDO_MAX = 10000;
+
     public static void main(String[] args) {
+
+        Tenedor[] tenedores = new Tenedor[NUM_FILOSOFOS];
+        Filosofo[] filosofos = new Filosofo[NUM_FILOSOFOS];
+
+        for(int i = 0; i < NUM_FILOSOFOS; i++)
+            tenedores[i] = new Tenedor("Tenedor " + i);
+
+        for(int i = 0; i < NUM_FILOSOFOS; i++)
+            filosofos[i] = new Filosofo("Filosofo " + i, (new Random()).nextInt(TIEMPO_PESAMIENTO_MAX)+1,(new Random()).nextInt(TIEMPO_COMIENDO_MAX)+1, tenedores[i], tenedores[(i+1)%NUM_FILOSOFOS]);
+
+        for(int i = 0; i < NUM_FILOSOFOS; i++)
+            filosofos[i].start();
+
+
 
     }
 }
